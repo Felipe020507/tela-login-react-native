17683f891eddc93be9368546019939a66f3f7d33
Felipe020507 <felipecasale75@gmail.com>
2024-02-06 14:18:02 -0300

    teste

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, Image, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+         <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png',
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Nome de Usuário:"
+      />
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Senha:"
+      />
+      <Button
+        title="Entrar"
+        onPress={() => {i
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +34,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 780,
+    height: 150,
+    marginBottom: 10,
+  },
+
+  input: {
+    height: 60,
+    backgroundColor: '#fff',
+    borderColor: '#000000',
+    borderWidth: 1,
+    justifyContent: 'center',
+  },
 });
+
+export default App;
